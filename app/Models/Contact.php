028f8258b98fecd214dd084885aef86dcf5fdfc4
Meder <medalon07@gmail.com>
2022-09-20 12:16:05 +0600

    global scope trait

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,14 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Scopes\FilterScope;
-use App\Scopes\ContactSearchScope;
+use App\Scopes\FilterSearchScope;
 
 class Contact extends Model
 {
-    use HasFactory;
+    use HasFactory, FilterSearchScope;
 
     protected $fillable = ['first_name', 'last_name', 'email', 'phone', 'address', 'company_id'];
+
+    public $searchColumns = ['first_name', 'last_name', 'email', 'company.name'];
 
     public $filterColumns = ['company_id'];
 
@@ -25,11 +26,4 @@ class Contact extends Model
         return $query->orderBy('id', 'desc');
     }
 
-    protected static function booted()
-    {
-        
-
-        static::addGlobalScope(new FilterScope);
-        static::addGlobalScope(new ContactSearchScope);
-    }
 }
---
+++ app/Scopes/FilterSearchScope.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Scopes;
+
+trait FilterSearchScope
+{
+    protected static function bootFilterSearchScope()
+    {
+        static::addGlobalScope(new SearchScope);
+        static::addGlobalScope(new FilterScope);
+    }
+
+}
