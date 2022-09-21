d041b8c66bbaee339580d307c412f0c66f87f48d
Meder <medalon07@gmail.com>
2022-09-21 15:26:39 +0600

    logout function in comtroller

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,6 @@ Route::put('/contacts/{id}', [ContactController::class, 'update'])->name('contac
 Route::delete('/contacts/{id}', [ContactController::class, 'destroy'])->name('contacts.destroy');
 
 Route::get('/contacts/edit/{id}', [ContactController::class, 'edit'])->name('contacts.edit');
+Auth::routes();
+
+Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
